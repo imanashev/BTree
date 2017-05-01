c440379a5bccd8137ac8e9b1d3f0a01e7a01c55d
Ildar Manashev <ildar404@gmail.com>
2017-05-01 22:23:20 +0700

    Erase in process

--- BTree/BNode.h
+++ BTree/BNode.h
@@ -1,59 +1,59 @@
 #pragma once
 #include "BTree.h"
 #include <cstdlib> 
-//#define  T BTree<ValueType>::t;
+//#define  T BTree<V>::t;
 
-template <class ValueType>
+template <class V>
 class BNode
 {
 private:
 	bool leaf;
 	int  nkeys;
 	int*  key;
-	ValueType *value;
-	BNode<ValueType> **child;
+	V *value;
+	BNode<V> **child;
 
 	BNode();
 	~BNode();
-	void insertNonfull(int _key, ValueType _value);
-	void splitNode(BNode<ValueType> * node, int index);
+	void insertNonfull(int _key, V _value);
+	void splitNode(BNode<V> * node, int index);
 
-	//ValueType eraseSrc(int _key);
-	//void erase(BNode<ValueType> * parent, int index);
+	//V eraseSrc(int _key);
+	//void erase(BNode<V> * parent, int index);
 
-	void mergeNode(BNode<ValueType> * node, int index);
-	void moveNode(BNode<ValueType> * node, int index);
+	void mergeNode(BNode<V> * node, int index);
+	void moveNode(BNode<V> * node, int index);
 
 	void traverse(int tab = 0);
-	bool search(int _key, ValueType* _value);
+	bool search(int _key, V* _value);
 
-	template <class ValueType>friend class BTree;
+	template <class V>friend class BTree;
 };
 
-template <class ValueType>
-BNode<ValueType>::BNode()
+template <class V>
+BNode<V>::BNode()
 {
 	leaf = true;
 	nkeys = 0;
-	//key = (int*)malloc(sizeof(int) * 2 * (BTree<ValueType>::t - 1));
-	//value = (ValueType**)malloc(sizeof(ValueType*) * 2 * (BTree<ValueType>::t - 1));
-	//child = (BNode<ValueType>**)malloc(sizeof(BNode<ValueType>*) * 2 * BTree<ValueType>::t);
+	//key = (int*)malloc(sizeof(int) * 2 * (BTree<V>::t - 1));
+	//value = (V**)malloc(sizeof(V*) * 2 * (BTree<V>::t - 1));
+	//child = (BNode<V>**)malloc(sizeof(BNode<V>*) * 2 * BTree<V>::t);
 
-	key = new int[2 * BTree<ValueType>::t - 1];
-	value = new ValueType [2 * BTree<ValueType>::t - 1];
-	child = new BNode<ValueType>* [2 * BTree<ValueType>::t];
+	key = new int[2 * BTree<V>::t - 1];
+	value = new V [2 * BTree<V>::t - 1];
+	child = new BNode<V>* [2 * BTree<V>::t];
 }
 
-template <class ValueType>
-BNode<ValueType>::~BNode()
+template <class V>
+BNode<V>::~BNode()
 {
 	//free(key);
 	//free(value);
 	//free(child);
 }
 
-template <class ValueType>
-void BNode<ValueType>::insertNonfull(int _key, ValueType _value)
+template <class V>
+void BNode<V>::insertNonfull(int _key, V _value)
 {
 	int i = nkeys - 1;
 	if (leaf)
@@ -75,7 +75,7 @@ void BNode<ValueType>::insertNonfull(int _key, ValueType _value)
 			i--;
 		}
 		i++;
-		if (child[i]->nkeys == 2 * BTree<ValueType>::t - 1)
+		if (child[i]->nkeys == 2 * BTree<V>::t - 1)
 		{
 			splitNode(child[i], i);
 			if (_key > key[i])
@@ -87,26 +87,26 @@ void BNode<ValueType>::insertNonfull(int _key, ValueType _value)
 	}
 }
 
-template <class ValueType>
-void BNode<ValueType>::splitNode(BNode<ValueType>* node, int index)
+template <class V>
+void BNode<V>::splitNode(BNode<V>* node, int index)
 {
 	int i;
-	BNode<ValueType> * z = new BNode<ValueType>;
+	BNode<V> * z = new BNode<V>;
 	z->leaf = node->leaf;
-	z->nkeys = BTree<ValueType>::t - 1;
-	for (i = 0; i < BTree<ValueType>::t - 1; i++)
+	z->nkeys = BTree<V>::t - 1;
+	for (i = 0; i < BTree<V>::t - 1; i++)
 	{
-		z->key[i] = node->key[BTree<ValueType>::t + i];
-		z->value[i] = node->value[BTree<ValueType>::t + i];
+		z->key[i] = node->key[BTree<V>::t + i];
+		z->value[i] = node->value[BTree<V>::t + i];
 	}
 	if (!node->leaf)
 	{
-		for (i = 0; i < BTree<ValueType>::t; i++)
+		for (i = 0; i < BTree<V>::t; i++)
 		{
-			z->child[i] = node->child[BTree<ValueType>::t + i];
+			z->child[i] = node->child[BTree<V>::t + i];
 		}
 	}
-	node->nkeys = BTree<ValueType>::t - 1;
+	node->nkeys = BTree<V>::t - 1;
 
 	for (i = nkeys; i >= index + 1; i--)
 	{
@@ -118,13 +118,13 @@ void BNode<ValueType>::splitNode(BNode<ValueType>* node, int index)
 		key[i + 1] = key[i];
 		value[i + 1] = value[i];
 	}
-	key[index] = node->key[BTree<ValueType>::t - 1];
-	value[index] = node->value[BTree<ValueType>::t - 1];
+	key[index] = node->key[BTree<V>::t - 1];
+	value[index] = node->value[BTree<V>::t - 1];
 	nkeys++;
 }
 
-template <class ValueType>
-void BNode<ValueType>::traverse(int tab)
+template <class V>
+void BNode<V>::traverse(int tab)
 {
 	int i;
 	for (i = 0; i < nkeys; i++)
@@ -144,8 +144,8 @@ void BNode<ValueType>::traverse(int tab)
 	}
 }
 
-template <class ValueType>
-bool BNode<ValueType>::search(int _key, ValueType* _value)
+template <class V>
+bool BNode<V>::search(int _key, V* _value)
 {
 	int i = 0;
 	while (i < nkeys && _key > key[i])
@@ -168,21 +168,21 @@ bool BNode<ValueType>::search(int _key, ValueType* _value)
 }
 
 
-//template<class ValueType>
-//void BNode<ValueType>::moveNode(BNode<ValueType>* parent, int index)
+//template<class V>
+//void BNode<V>::moveNode(BNode<V>* parent, int index)
 //{
-//	if (parent->child[index + 1] >= BTree<ValueType>::t)
+//	if (parent->child[index + 1] >= BTree<V>::t)
 //	{
 //
 //	}
-//	else if (parent->child[index - 1] >= BTree<ValueType>::t)
+//	else if (parent->child[index - 1] >= BTree<V>::t)
 //	{
 //
 //	}
 //}
 
-//template <class ValueType>
-//bool BNode<ValueType>::eraseSrc(int _key)
+//template <class V>
+//bool BNode<V>::eraseSrc(int _key)
 //{
 //	int i = 0;
 //	while (i < nkeys && _key > key[i])
@@ -201,12 +201,12 @@ bool BNode<ValueType>::search(int _key, ValueType* _value)
 //	return child[i]->erase(_key);
 //}
 //
-//template <class ValueType>
-//void BNode<ValueType>::erase(BNode<ValueType> * parent,int index)
+//template <class V>
+//void BNode<V>::erase(BNode<V> * parent,int index)
 //{
 //	if (leaf)
 //	{
-//		if (nkeys > BTree<ValueType>::t - 1)
+//		if (nkeys > BTree<V>::t - 1)
 //		{
 //			for (int i = index; i < nkeys - 1; i++)
 //			{
--- BTree/BTree.h
+++ BTree/BTree.h
@@ -2,44 +2,44 @@
 #include "BNode.h"
 #include "cstddef"
 
-template <class ValueType>
+template <class V>
 class BTree
 {
 private:
-	BNode<ValueType> * root;
+	BNode<V> * root;
 	static const int t = 4;
 public:
 	BTree();
 	BTree(int _t);
 	static int getT();
 
-	void insert(int key, ValueType& value);
+	void insert(int key, V& value);
 	void traverse();
-	bool search(int key, ValueType* _value = NULL);
+	bool search(int key, V* _value = NULL);
 	bool erase(int key);
-	//void showHelper(BNode<ValueType> * node);
+	//void showHelper(BNode<V> * node);
 
-	template <class ValueType> friend class BNode;
+	template <class V> friend class BNode;
 };
 
-template<class ValueType>
-inline BTree<ValueType>::BTree() : root(NULL) {};
+template<class V>
+inline BTree<V>::BTree() : root(NULL) {};
 
-template<class ValueType>
-inline BTree<ValueType>::BTree(int _t) : t(_t), root(NULL) {};
+template<class V>
+inline BTree<V>::BTree(int _t) : t(_t), root(NULL) {};
 
-template <class ValueType>
-int BTree<ValueType>::getT()
+template <class V>
+int BTree<V>::getT()
 {
 	return t;
 };
 
-template<class ValueType>
-void BTree<ValueType>::insert(int key, ValueType& value)
+template<class V>
+void BTree<V>::insert(int key, V& value)
 {
 	if (root == NULL)
 	{
-		root = new BNode<ValueType>;
+		root = new BNode<V>;
 		root->nkeys = 1;
 		root->leaf = true;
 		root->key[0] = key;
@@ -49,7 +49,7 @@ void BTree<ValueType>::insert(int key, ValueType& value)
 	{
 		if (root->nkeys == 2 * t - 1)
 		{
-			BNode<ValueType>* newroot = new BNode<ValueType>;
+			BNode<V>* newroot = new BNode<V>;
 			newroot->leaf = false;
 			newroot->nkeys = 0;
 			newroot->child[0] = root;
@@ -69,8 +69,8 @@ void BTree<ValueType>::insert(int key, ValueType& value)
 	}
 }
 
-template<class ValueType>
-void BTree<ValueType>::traverse()
+template<class V>
+void BTree<V>::traverse()
 {
 	if (root != NULL)
 	{
@@ -78,8 +78,8 @@ void BTree<ValueType>::traverse()
 	}
 }
 
-template<class ValueType>
-bool BTree<ValueType>::search(int key, ValueType* _value)
+template<class V>
+bool BTree<V>::search(int key, V* _value)
 {
 	if (root != NULL)
 	{
@@ -90,44 +90,162 @@ bool BTree<ValueType>::search(int key, ValueType* _value)
 
 
 //TODO erase from habr
-template<class ValueType>
-bool BTree<ValueType>::erase(int _key)
+template<class V>
+bool BTree<V>::erase(int _key)
 {
 	if (root == NULL) //Дерево пусто
 	{
 		return 0;
 	}
-	BNode<ValueType>* parent = NULL;
-	BNode<ValueType>* cur = root;
-	int i;
+	/* Часть поиска */
+	BNode<V>* parent = NULL;
+	BNode<V>* node = root;
+	int nodeNo; // Индекс удаляемого элемента
+	int parentNo; // Индекс node в parent
 	while(true) // Поиск нужного элемента
 	{
-		i = 0;
-		while (i < cur->nkeys && _key > cur->key[i])
+		nodeNo = 0;
+		while (nodeNo < node->nkeys && _key > node->key[nodeNo])
 		{
-			i++;
+			nodeNo++;
 		}
-		if (cur->key[i] == _key) //Ключ найден
+		if (node->key[nodeNo] == _key) //Ключ найден
 		{
 			break;
 		}
-		if (cur->leaf) // Ключ не найден
+		if (node->leaf) // Ключ не найден
 		{
 			return 0;
 		}
-		parent = cur;
-		cur = cur->child[i];
+		parent = node;
+		parentNo = nodeNo;
+		node = node->child[nodeNo];
 	}
-	if (cur->leaf)
-	{
-		if (cur->nkeys > t - 1) //Просто удаляем
-		{
 
-		}
-		else
+	/* Часть удаления */
+	if (node->leaf) // Из листа
+	{
+		if (node->nkeys > t - 1) //Ключей больше t-1: Просто удаляем
 		{
-			if() // Есть соседний
+			delete node->value[nodeNo];
+
+			for (int i = nodeNo; i < node->nkeys - 1; i++)
+			{
+				node->key[i] = node->key[i + 1];
+				node->value[i] = node->value[i + 1];
+			}
+			node->nkeys--;
 		}
+		else // Ключей меньше t-1
+		{
+			bool first, last;
+			last = parentNo != parent->nkeys - 1;
+			first = parentNo != 0;
+			BNode<V>* neighbor = NULL;
+
+			// Есть соседний c nkeys > t-1
+			if (!last)
+			{
+				if (parent->child[parentNo + 1]->nkeys > t - 1) // Сосед справа
+				{
+					/* если существует соседний лист (находящийся рядом с ним
+					и имеющий такого же родителя), который содержит больше t-1
+					ключа, то выберем ключ из этого соседа, который является
+					разделителем между оставшимися ключами узла-соседа и
+					исходного узла (то есть не больше всех из одной группы и
+					не меньше всех из другой). Пусть это ключ k1. Выберем
+					ключ k2 из узла-родителя, который является разделителем
+					исходного узла и его соседа, который мы выбрали ранее.
+					Удалим из исходного узла нужный ключ (который необходимо
+					было удалить), спустим k2 в этот узел, а вместо k2 в
+					узле-родителе поставим k1.
+					*/
+					neighbor = parent->child[parentNo + 1];
+
+					delete node->value[nodeNo];
+					for (int i = nodeNo; i < node->nkeys - 1; i++)
+					{
+						node->key[i] = node->key[i + 1];
+						node->value[i] = node->value[i + 1];
+					}
+					node->key[nkeys - 1] = parent->key[parentNo];
+					node->value[nkeys - 1] = parent->value[parentNo];
+
+					parent->key[parentNo] = neighbor->key[0];
+					parent->value[parentNo] = neighbor->value[0];
+
+					for (int i = 0; i < neighbor->nkeys - 1; i++)
+					{
+						neighbor->key[i] = neighbor->key[i + 1];
+						neighbor->key[i] = neighbor->key[i + 1];
+					}
+					neighbor->nkeys--;
+					first = 1;
+				}
+			}
+			if (!first)
+			{
+				if (parent->child[parentNo - 1]->nkeys > t - 1) // Сосед слева
+				{
+					neighbor = parent->child[parentNo - 1];
+
+					delete node->value[nodeNo];
+					for (int i = nodeNo; i > 0; i--)
+					{
+						node->key[i] = node->key[i - 1];
+						node->value[i] = node->value[i - 1];
+					}
+					node->key[0] = parent->key[parentNo];
+					node->value[0] = parent->value[parentNo];
+
+					parent->key[parentNo] = neighbor->key[nkeys - 1];
+					parent->value[parentNo] = neighbor->value[nkeys - 1];
+
+					neighbor->nkeys--;
+				}
+			}
+			if (neighbor) // Все соседи по t-1 ключу
+			{
+				/* Если же все соседи нашего узла имеют по t-1 ключу.
+				То мы объединяем его с каким-либо соседом, удаляем нужный
+				ключ. И тот ключ из узла-родителя, который был разделителем
+				для этих двух «бывших» соседей, переместим в наш
+				новообразовавшийся узел (очевидно, он будет в нем медианой).
+				*/
+				if (parentNo == 0) // Объединяем с правым соседом
+				{
+					///TODO
+				}
+				else // Объединяем с левым соседом
+				{
+					///TODO
+				}
+			}
+		}
+	}
+	else // Не лист
+	{
+		if () // Если дочерний узел, предшествующий ключу k содержит больше t-1 ключа
+		{
+			/* то находим k1 – предшественника k в поддереве этого узла. 
+			Удаляем его (рекурсивно запускаем наш алгоритм). Заменяем k 
+			в исходном узле на k1. 
+			
+			Проделываем аналогичную работу, если дочерний узел, следующий за ключом k,
+			имеет больше t-1 ключа.
+			*/
+		}
+		else if () //Если оба (следующий и предшествующий дочерние узлы) имеют по t-1 ключу,
+		{
+			/*то объединяем этих детей, переносим в них k, а далее удаляем k из нового
+			узла(рекурсивно запускаем наш алгоритм).*/
+		}
+		else if () //Если сливаются 2 последних потомка корня
+		{
+			/*то они становятся корнем,	а предыдущий корень освобождается.
+			*/
+		}
+
 	}
 
 
